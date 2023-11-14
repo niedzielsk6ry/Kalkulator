80a6c400e84370fe57e289076b6fe7bb54c8dda7
Lukasz Niedzielski <niedzielskilukasz92@gmail.com>
2023-11-14 13:23:22 +0100

    first commit

---
+++ src/main/java/com/calculator/Calculator.java
@@ -0,0 +1,15 @@
+package com.calculator;
+
+public class Calculator {
+    public void jakasMetoda() {
+        System.out.println("działa");
+    }
+    public void jakasMetoda2() {
+        System.out.println("też działa");
+    }
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+                calculator.jakasMetoda();
+    }
+}
